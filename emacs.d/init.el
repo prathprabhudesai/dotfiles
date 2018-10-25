23e46d2bb05826aafa3b9daa8b82928ff8b56556
Prathamesh Prabhudesai <prathamesh.prabhudesai@nuagenetworks.net>
2018-10-25 17:25:03 -0400

    setting the default theme at the startup

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -31,6 +31,7 @@
      ido-vertical-mode
      smex
      company
+     color-theme-sanityinc-tomorrow
      )
   )
 
@@ -361,24 +362,21 @@
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
 
+;;---------- THEMES ---------------
+
+(require 'color-theme-sanityinc-tomorrow)
+(load-theme 'sanityinc-tomorrow-bright t)
+
 ;; custom variables (added automatically + manual)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes (quote (dracula)))
  '(custom-safe-themes
    (quote
-    ("aaffceb9b0f539b6ad6becb8e96a04f2140c8faa1de8039a343a4f1e009174fb" default)))
- '(grep-command "grep -nH -i -r ")
- '(package-selected-packages
-   (quote
-    (company-c-headers darkroom auto-auto-indent aggressive-indent emamux xcscope smex markdown-mode ido-vertical-mode dracula-theme company color-theme autopair auto-complete))))
+    ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
+ '(grep-command "grep -nH -i -r "))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
