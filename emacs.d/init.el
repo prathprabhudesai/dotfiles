81bd5b5240adb2a513613d188989f1632b888696
Prathamesh Prabhudesai <prathprabhudesai@gmail.com>
2021-05-18 13:31:43 -0700

    Update init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -155,34 +155,6 @@
 (require 'py-autopep8)
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
 
-;; Setup pdf-tools
-(require 'use-package)
-(use-package pdf-tools
-  :pin manual
-  :init (pdf-tools-install)
-  :bind (:map pdf-view-mode-map
-			  ("T" . pdf-annot-add-text-annotation)
-			  ("D" . pdf-annot-delete)
-			  ("t" . pdf-annot-add-highlight-markup-annotation)
-			  ("j" . image-next-line)
-			  ("k" . image-previous-line)
-			  ("l" . image-forward-hscroll)
-			  ("h" . image-backward-hscroll)
-			  ("G" . pdf-view-last-page)
-			  ("g" . nil)
-			  ("gg" . pdf-view-first-page)
-			  ("C-c C-c" . image-toggle-display)
-			  ("C-s" . isearch-forward))
-  :config
-  (setq-default pdf-view-display-size 'fit-page)
-  :custom
-  (yas-minor-mode nil)
-  (pdf-cache-image-limit 32)
-  (pdf-view-max-image-width 2048)
-  (pdf-view-resize-factor 1.8)
-  (pdf-isearch-batch-mode t)
-  (pdf-annot-activate-created-annotations t))
-
 ;;C style 
 
 (defun config-indent-80andNoTrail()
